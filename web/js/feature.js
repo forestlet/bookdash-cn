64dcfb89dca5078a6de08f1b64d50131ddc56ee8
forestlet <hyf2021@outlook.com>
2021-04-02 10:01:17 +0800

    fix page bug

--- web/js/feature.js
+++ web/js/feature.js
@@ -1,4 +1,4 @@
-// get 3 random numbers
+// get random numbers
 function randomNums(n, min, max) {
     var arr = [];
     for (i = 0; i < n; i++) {
@@ -18,4 +18,82 @@ function isExist(arr, ran) {
         }
     }
     return false;
+}
+
+// change object data, so can read online
+$("#viewer").on('show.bs.modal', (event) => {
+    let button = event.relatedTarget
+    let file = button.getAttribute('data-bs-file')
+    let obj = "<object type='application/pdf' data='" + file + "' id='review' style='width: 100%; height: 100%;'></object>";
+    $(".modal-body").append(obj)
+})
+$("#viewer").on('hide.bs.modal', () => {
+    $(".modal-body").empty()
+})
+
+// change page-item style
+function stylePage(current_page, page_len) {
+    $("li.page-item").attr("class", "page-item")
+    // page active & disable
+    if (current_page == 1) { $("li.page-item").eq(0).attr("class", "page-item disabled") }
+    if (current_page == page_len) { $("li.page-item").eq(page_len + 1).attr("class", "page-item disabled") }
+    $("li.page-item").eq(current_page).attr("class", "page-item active")
+}
+
+// update carousels
+function showCarousels(data, book_num) {
+    console.log("showCarousels");
+
+    random_nums = randomNums(3, -1, book_num - 1)
+    for (let j = 0; j < 3; j++) {
+        num = random_nums[j]
+        let this_book = data["info"][data["books"][num]]
+        this_carousel = $("div.carousel-item").eq(j)
+        this_carousel.find("img").attr("src", "../asset/" + this_book.cover)
+        this_carousel.find("img").attr("alt", this_book.book_cn)
+        this_carousel.find("h1").text(this_book.book_cn)
+        this_carousel.find("p").eq(0).text(this_book.contents)
+        this_carousel.find("button").attr("data-bs-toggle", "modal")
+        this_carousel.find("button").attr("data-bs-target", "#viewer")
+        this_carousel.find("button").attr("data-bs-file", "../asset/" + this_book.file)
+        this_carousel.find("a").eq(0).attr("href", "../asset/" + this_book.file)
+        this_carousel.find("a").eq(0).attr("download", "../asset/" + this_book.file)
+    }
+}
+
+// update cards
+function showCards(data, book_num) {
+    console.log("showCards");
+
+    let current_page = localStorage.getItem("page")
+
+    // clean up
+    for (let j = 0; j < 12; j++) {
+        this_card = $("div.card").eq(j)
+        this_card.find("img").attr("src", "")
+        $("div.card").eq(j).show()
+    }
+
+    for (let i = (current_page - 1) * 12, j = 0; i < Math.min(book_num, current_page * 12); i++, j++) {
+        let this_book = data["info"][data["books"][i]]
+        this_card = $("div.card").eq(j)
+        console.log(this_card);
+        this_card.find("img").attr("src", "../asset/" + this_book.cover)
+        this_card.find("img").attr("alt", this_book.book_cn)
+        this_card.find("h3").text(this_book.book_cn)
+        this_card.find("p.card-text").text(this_book.contents)
+        this_card.find("button").eq(0).attr("data-bs-toggle", "modal")
+        this_card.find("button").eq(0).attr("data-bs-target", "#viewer")
+        this_card.find("button").eq(0).attr("data-bs-file", "../asset/" + this_book.file)
+        this_card.find("small.text-muted").text(this_book.date)
+        this_card.find("button").eq(1).find("a").attr("href", "../asset/" + this_book.file)
+        this_card.find("button").eq(1).find("a").attr("download", "../asset/" + this_book.file)
+    }
+
+    // remaining cards
+    for (let k = 0; k < 12; k++) {
+        if ($("div.card").find("img").eq(k).attr("src") == "") {
+            $("div.card").eq(k).hide()
+        }
+    }
 }
