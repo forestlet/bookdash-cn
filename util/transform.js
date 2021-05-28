01d315cfa97e19eb885fc6229159b42c251521a6
forestlet <hyf2021@outlook.com>
2021-05-28 22:29:14 +0800

    refactor directory

--- util/transform.js
+++ util/transform.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 
 // read file
-let raw = fs.readFileSync("booklist.json");
+let raw = fs.readFileSync("../doc/booklist.json");
 let booklist_json = JSON.parse(raw);
 let booklist_map = new Map(Object.entries(booklist_json));
 let book_keys = booklist_map.keys();
@@ -12,13 +12,13 @@ let book_names = [...book_keys];
 // write to file
 let content = "# book list\n\n| 状态 Status | 绘本英文名 English Name  | 绘本中文名 Chinese Name | 译者 Translator | 日期 Date |\n\
 | ----------- | ------------------------ | ----------------------- | --------------- | --------- |\n";
-fs.writeFileSync('booklist.md', content)
+fs.writeFileSync('../doc/booklist.md', content)
 
 for (let index = 0; index < booklist_map.size; index++) {
     let this_book = booklist_map.get(book_names[index])
     content = `|✅|${book_names[index]}|${this_book["book_cn"]}|${this_book["translator"]}|${this_book["date"]}|\n`
 
-    fs.appendFileSync('booklist.md', content, err => {
+    fs.appendFileSync('../doc/booklist.md', content, err => {
         if (err) {
             console.error(err)
             return
